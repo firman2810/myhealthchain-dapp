64df4e3af237a8623565f0bad8bb2f61785c2d83
firman <firmaneymar@gmail.com>
2026-02-18 16:35:35 +0800

    init spring boot

---
+++ backend/src/main/java/com/firman/myhealthchain/MyhealthchainApplication.java
@@ -0,0 +1,15 @@
+package com.firman.myhealthchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhealthchainApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MyhealthchainApplication.class, args);
+    }
+
+}
